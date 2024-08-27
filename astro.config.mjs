ed107e63ee1b1d319518178873e68d762043739f
Iskander <iskander.sierra@gmail.com>
2024-08-27 10:39:42 +0200

    chore: Update tsconfig.json and package.json for Preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [preact()]
 });
