1e6b21895ecd58fba0f475fd4e504855257711b2
Iskander <iskander.sierra@gmail.com>
2024-08-27 15:52:44 +0200

    feat: Update RSS feed with dynamic content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,21 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getPostList } from "./tags/utils";
 
 export async function GET(context) {
+    const posts = await getPostList();
+
+    const items = posts.map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `/posts/${post.slug}`,
+    }));
+
     return rss({
         title: 'Astro Learner | Blog',
         description: 'My journey learning Astro',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items,
         customData: `<language>en-us</language>`,
     })
 }
